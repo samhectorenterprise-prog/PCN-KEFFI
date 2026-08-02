6f02edea94aca7316e3ab74eaf1421cd05f8e547
Sam Hector Tech <samhectorenterprise@gmail.com>
2026-08-02 11:53:30 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+    function showSidebar(){
+      const sideBar = document.querySelector(".sidebar").style.display = "flex"
+    }
+    function hideSidebar(){
+      const sideBar = document.querySelector(".sidebar").style.display = "none"
+    }
+
+    
